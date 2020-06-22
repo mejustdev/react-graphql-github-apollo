8326b42441ae2887c4275d97f4d0d9b0f09670f4
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-06-22 06:28:26 +0200

    add Issues setup

--- src/Repository/RepositoryList/index.js
+++ src/Repository/RepositoryList/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 
 import RepositoryItem from '../RepositoryItem';
 import FetchMore from '../../FetchMore';
+import Issues from '../../Issue';
 
 import '../style.css';
 
@@ -30,6 +31,7 @@ const RepositoryList = ({ repositories, fetchMore, loading, entry }) => (
     {repositories.edges.map(({ node }) => (
       <div key={node.id} className='RepositoryItem'>
         <RepositoryItem {...node} />
+        <Issues repositoryName={node.name} repositoryOwner={node.owner.login} />
       </div>
     ))}
     <FetchMore
