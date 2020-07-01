97419e3e931d43fa372b6743b99dda8cf5734e48
20twa07 <48910851+20twa07@users.noreply.github.com>
2020-07-02 00:25:37 +0200

    Updates

---
+++ precache-manifest.8b699b72b9bb50f13662fee8121c0f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290cdecf948633fbc38ecae12d3c3b5a",
+    "url": "/react-graphql-github-apollo/index.html"
+  },
+  {
+    "revision": "8160aff60b667ece5401",
+    "url": "/react-graphql-github-apollo/static/css/main.80dd43f4.chunk.css"
+  },
+  {
+    "revision": "3f2c26f0f2a68a1339c6",
+    "url": "/react-graphql-github-apollo/static/js/2.49275a19.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react-graphql-github-apollo/static/js/2.49275a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8160aff60b667ece5401",
+    "url": "/react-graphql-github-apollo/static/js/main.3bdc18af.chunk.js"
+  },
+  {
+    "revision": "70505bb45efb7f13b76f",
+    "url": "/react-graphql-github-apollo/static/js/runtime-main.cd3e0421.js"
+  }
+]);
